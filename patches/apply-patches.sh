c573f68fd6ff5e3f505c8acb35449921d219e73d
FakeMichau <49685661+FakeMichau@users.noreply.github.com>
2024-06-04 17:18:11 +0200

    Add Proton-LFX2 patches

---
+++ patches/apply-patches.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+git checkout -- proton
+echo "Proton: Add patches"
+patch -Np1 < ./patches/proton.patch
+patch -Np1 < ./patches/makefile.patch
+
+pushd dxvk || exit
+git reset --hard HEAD
+git clean -xdf
+echo "DXVK: Add LFX2 support"
+patch -Np1 < ../patches/lfx2-dxvk.patch
+popd || exit
+
+pushd vkd3d-proton || exit
+git reset --hard HEAD
+git clean -xdf
+echo "VKD3D-Proton: Add LFX2 support"
+patch -Np1 < ../patches/lfx2-vkd3d.patch
+popd || exit
+
+pushd wine || exit
+git reset --hard HEAD
+git clean -xdf
+echo "Wine: Add HAGS spoof"
+patch -Np1 < ../patches/wine-hags-spoof.patch
+popd || exit
+
+pushd dxvk-nvapi || exit
+git reset --hard HEAD
+git clean -xdf
+echo "DXVK-NVAPI: Add LFX2 support"
+patch -Np1 < ../patches/lfx2-nvapi.patch
+popd || exit
